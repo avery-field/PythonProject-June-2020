93d246fcd104fdc136dc285d0af4d96d762e249a
Avery Field <amf299@scarletmail.rutgers.edu>
2020-06-10 09:23:08 -0400

    code added

---
+++ Code/python-project-june-2020.py
@@ -0,0 +1,19 @@
+#imports
+import re
+import numpy as np
+import pandas as pd
+from pprint import pprint
+
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+import spacy
+
+# NLTK Stop words
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+
+#import dataset
